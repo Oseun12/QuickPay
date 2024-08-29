1d639632fd6c54a0bfc43515118bd36a5c7b9776
Seun Salau <156671873+Oseun12@users.noreply.github.com>
2024-08-29 21:43:12 +0100

    Chore: updating passport error

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -25,7 +25,7 @@ passport.use(new GoogleStrategy({
 }));
 
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+    done(null, user);
 });
 
 passport.deserializeUser((id, done) => {
